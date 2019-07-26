596b7c67146fb85765dc3d0d3cbb492a2b0175ca
Dagg <theamazingdoge91@gmail.com>
2019-07-26 13:39:21 -0700

    Random Fox

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 const {app, BrowserWindow} = require('electron')
+const path = require('path')
 
 function createWindow()
 {
@@ -7,7 +8,7 @@ function createWindow()
         height: 900,
         autoHideMenuBar: true
     })
-    win.loadURL("http://localhost:3000/")
+    win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`)
 }
 
 app.on("ready", createWindow)
--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={'https://dagg.xyz/randomfox/images/' + (Math.floor(Math.random() * 124) + 1) + ".jpg"} className="fox" alt="Fox!" />
     </div>
   );
 }
